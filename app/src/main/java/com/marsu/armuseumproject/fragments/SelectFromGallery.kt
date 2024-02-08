5dfceaefc3b48562db073858d172040019a37b7c
Sirja Kosonen <sirja.kosonen@gmail.com>
2024-02-08 09:48:24 +0200

    Add ViewCompositionStrategy
    DisposeOnViewTreeLifecycleDestroyed strategy is the best practice when using ComposeView in fragments.
    
    https://developer.android.com/codelabs/jetpack-compose-migration#9

--- app/src/main/java/com/marsu/armuseumproject/fragments/SelectFromGallery.kt
+++ app/src/main/java/com/marsu/armuseumproject/fragments/SelectFromGallery.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -62,9 +63,12 @@ class SelectFromGallery : Fragment() {
             container,
             false
         ).apply {
-            composeView.setContent {
-                MaterialTheme {
-                    SelectFromGalleryScreen(viewModel = viewModel)
+            composeView.apply {
+                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+                setContent {
+                    MaterialTheme {
+                        SelectFromGalleryScreen(viewModel = viewModel)
+                    }
                 }
             }
         }
