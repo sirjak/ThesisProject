b9cf2be4f9a12f2bd54ac2f1bb3f4a622c75dd10
Sirja Kosonen <sirja.kosonen@gmail.com>
2024-02-20 12:50:56 +0200

    Adjust Popup contents
    Elementa are now arranged way better but BACK button is still left out of the area on small screens.

--- app/src/main/java/com/marsu/armuseumproject/ui_components/ArtPopup.kt
+++ app/src/main/java/com/marsu/armuseumproject/ui_components/ArtPopup.kt
@@ -4,9 +4,10 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -23,6 +24,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.core.net.toUri
 import coil.compose.rememberAsyncImagePainter
 import com.marsu.armuseumproject.R
@@ -33,23 +35,6 @@ import com.marsu.armuseumproject.database.Artwork
 @Composable
 fun ArtPopup(art: Artwork, onDismiss: () -> Unit) {
     val imageUri = art.primaryImage.toUri()
-    /*Popup(
-        alignment = Alignment.Center,
-        onDismissRequest = { Log.d("DISMISS", "Dismiss request detected") }, content = {
-            Card(
-                colors = CardColors(
-                    containerColor = MaterialTheme.colorScheme.error,
-                    contentColor = MaterialTheme.colorScheme.primary,
-                    disabledContainerColor = MaterialTheme.colorScheme.secondary,
-                    disabledContentColor = MaterialTheme.colorScheme.tertiary
-                ), modifier = Modifier
-                    .fillMaxSize()
-                    .padding(start = 20.dp, end = 20.dp, top = 60.dp, bottom = 60.dp)
-            ) {
-                Text(text = "At least this works")
-            }
-        })*/
-    Log.d("POPUP art", art.toString())
 
     Dialog(
         content = {
@@ -61,66 +46,71 @@ fun ArtPopup(art: Artwork, onDismiss: () -> Unit) {
                     disabledContentColor = MaterialTheme.colorScheme.tertiary
                 ),
                 modifier = Modifier
-                    .padding(start = 20.dp, end = 20.dp, top = 120.dp, bottom = 147.dp)
-                    .fillMaxSize()
-                //.align(alignment = Alignment.CenterHorizontally)
+                    .fillMaxWidth(0.90f)
+                    .fillMaxHeight(0.80f),
+                shape = MaterialTheme.shapes.medium
             ) {
-                /*Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Top
-                ) {*/
-                Image(
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.height(197.dp),
-                    painter = rememberAsyncImagePainter(imageUri),
-                    contentDescription = null
-                )
-                Button(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .offset(y = (-4).dp),
-                    onClick = { /*TODO*/ },
-                    shape = MaterialTheme.shapes.extraSmall
-                ) {
-                    Text(text = stringResource(id = R.string.save_image))
-                }
-
                 Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(top = 60.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Top
+                    modifier = Modifier.fillMaxSize(),
+                    //horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text(fontWeight = FontWeight.Bold, text = art.title)
-                    HorizontalDivider(
-                        modifier = Modifier.padding(
-                            horizontal = 32.dp,
-                            vertical = 16.dp
-                        )
-                    )
-                    Text(
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(bottom = 60.dp),
-                        text = art.artistDisplayName
+                    Image(
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxWidth().fillMaxHeight(0.50f),
+                        painter = rememberAsyncImagePainter(imageUri),
+                        contentDescription = null
                     )
                     Button(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .align(Alignment.CenterHorizontally),
-                        onClick = { onDismiss() },
+                            .offset(y = (-4).dp),
+                        onClick = { /*TODO*/ },
                         shape = MaterialTheme.shapes.extraSmall
                     ) {
-                        Text(text = stringResource(id = R.string.back))
+                        Text(text = stringResource(id = R.string.save_image))
                     }
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        //.fillMaxSize()
+                        //.padding(top = 60.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Spacer(modifier = Modifier.fillMaxHeight(0.20f))
+                        Text(fontWeight = FontWeight.Bold, text = art.title)
+                        HorizontalDivider(
+                            modifier = Modifier.padding(
+                                horizontal = 32.dp,
+                                vertical = 16.dp
+                            )
+                        )
+                        Text(
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(bottom = 60.dp),
+                            text = art.artistDisplayName
+                        )
+                        Spacer(modifier = Modifier.fillMaxHeight(0.20f))
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .offset(y = 4.dp),
+                            //.weight(2f, false),
+                            onClick = { onDismiss() },
+                            shape = MaterialTheme.shapes.extraSmall
+                        ) {
+                            Text(text = stringResource(id = R.string.back))
+                        }
+                    }
+
                 }
-                //}
             }
         },
         onDismissRequest = {
             Log.d("DISMISS", "Dismiss request detected")
             onDismiss()
-        })
+        },
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    )
 
 }
