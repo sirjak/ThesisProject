e0b0bc10bd5bada90fd803d8e6bf9e3c2b8d1bcb
Sirja Kosonen <sirja.kosonen@gmail.com>
2024-02-22 10:30:40 +0200

    Selection functionality
    Changes:
    - Radio buttons can now be selected
    - Initial state is always no selection
    - Reset button returns the sate to no selection

--- app/src/main/java/com/marsu/armuseumproject/ui_components/SelectDepartmentPopup.kt
+++ app/src/main/java/com/marsu/armuseumproject/ui_components/SelectDepartmentPopup.kt
@@ -5,24 +5,31 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.window.Dialog
 import com.marsu.armuseumproject.R
 import com.marsu.armuseumproject.database.Datasource
+import com.marsu.armuseumproject.database.Department
 
 @Composable
 fun SelectDepartmentPopup(onDismiss: () -> Unit) {
     // TODO: Add all the elements and functionality
     val options = Datasource().loadDepartments()
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf<Department?>(null) }
 
     Dialog(
         content = {
@@ -44,7 +51,20 @@ fun SelectDepartmentPopup(onDismiss: () -> Unit) {
                         .weight(0.94f)
                 ) {
                     itemsIndexed(options) { _, option ->
-                        DepartmentItem(department = option)
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .selectable(
+                                    selected = (option == selectedOption),
+                                    onClick = { onOptionSelected(option) },
+                                    role = Role.RadioButton
+                                ),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(selected = (option == selectedOption), onClick = { /*TODO*/ })
+                            Text(text = stringResource(id = option.stringResourceId))
+                        }
+                        //DepartmentItem(department = option)
                     }
                 }
 
@@ -56,7 +76,7 @@ fun SelectDepartmentPopup(onDismiss: () -> Unit) {
                 ) {
                     Button(
                         modifier = Modifier.fillMaxWidth(0.5f),
-                        onClick = { /*TODO*/ },
+                        onClick = { onOptionSelected(null) },
                         shape = MaterialTheme.shapes.extraSmall
                     ) {
                         Text(text = stringResource(id = R.string.reset))
